7427be3d9dce914dd1169db9e3302b0b21bba59b
Julia "jubsz <JuliaAmaralSantos@outlook.com>
2024-06-18 10:52:23 -0300

    feat: aprimorei o callback

--- jubsz.js
+++ jubsz.js
@@ -1,13 +1,27 @@
-let pizza;
+function orderFood(data, callback1, callback2) {
+  console.log("Ordering some food...");
 
-function orderPizza(){
-    console.log('Order pizza');
-    setTimeout(() => {
-        pizza = '🍕';
-    }, 2000);
-    console.log('Pizza was ordered');
+  setTimeout(() => {
+    console.log("Calling Soul ❤️");
+  }, 1000);
+
+  setTimeout(() => {
+    callback1(data);
+  }, 2000);
+
+  setTimeout(() => {
+    callback2(data);
+  }, 5000);
 }
 
-orderPizza();
+function foodPreparing(food) {
+  console.log(`IFOOD: Preparing ${food}...`);
+}
 
-console.log(`Eat ${pizza}`);
+function foodReady(food) {
+  console.log(`Eat the ${food}`);
+}
+
+const food = "🍪";
+
+orderFood(food, foodPreparing, foodReady);
