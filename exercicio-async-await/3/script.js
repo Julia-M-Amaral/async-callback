b43dac1430c1d77245e73d6f48113c3a779e5374
Julia "jubsz <JuliaAmaralSantos@outlook.com>
2024-06-25 15:49:00 -0300

    simulador de busca de dados numa rede social

---
+++ exercicio-async-await/3/script.js
@@ -0,0 +1,58 @@
+class SocialMedia{
+    constructor(posts, comments){
+        this.posts = posts;
+        this.comments = comments;
+    }
+}
+
+const posts = ["Post 1 Sunday", "Post 2 Thursday"];
+const comments = ["Cool! I've watched this series before and I love it.", "Congratulations, Ana!"];
+
+class Person extends SocialMedia{
+    constructor(name,age,profession){
+        super(posts, comments)
+        this.name = name;
+        this.age = age;
+        this.profession = profession
+    }
+}
+
+const p1 = new Person("Julia", 21, "Intern");
+
+function fetchUserData(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve (`Name: ${p1.name}, ${p1.age} years old, ${p1.profession} at Avanade.`);
+        }, 2000);
+    });
+}
+
+function fetchUserPosts(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`${p1.posts.join(", ")}`);
+        }, 3000);
+    });
+}
+
+function fetchUserComments(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`${p1.comments.join(", ")}`);
+        }, 1500);
+    });
+}
+
+async function loadUserDetails(){
+    const resultUserData = await fetchUserData();
+    const resultUserPosts = await fetchUserPosts();
+    const resultUserComments = await fetchUserComments(); 
+
+    console.log(`
+        Personal data: ${resultUserData}
+        Posts: ${resultUserPosts}
+        Comments: ${resultUserComments}
+        `)
+}
+
+loadUserDetails();
