3d59f37c17292cd6af3075d51169dd63a0fa5389
Julia "jubsz <JuliaAmaralSantos@outlook.com>
2024-06-25 12:17:17 -0300

    bsucador de lista de frutas

--- exercicio-async-await/2/script.js
+++ exercicio-async-await/2/script.js
@@ -4,4 +4,40 @@ const fruits = {
     purple: ['Fig', 'Plum', 'Grape', 'Blueberry']
 }
 
-console.log(fruits.purple);
+
+function fetchRedFruits(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve (`Red fruits: ${fruits.red.join(", ")}`);
+        }, 1000);
+    });
+}
+
+function fetchYellowFruits(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve (`Yellow fruits: ${fruits.yellow.join(", ")}`);
+        }, 2000);
+    });
+}
+
+function fetchPurpleFruits(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve (`Purple fruits: ${fruits.purple.join(", ")}`);
+        }, 3000);
+    });
+}
+
+async function loadAllData(){
+    const data2 = await fetchYellowFruits();
+    console.log(data2);
+
+    const data3 = await fetchPurpleFruits();
+    console.log(data3);
+
+    const data1 = await fetchRedFruits();   
+    console.log(data1);
+}
+
+loadAllData();
